241ab82526571acc33a48b2aa796327bcd8c0472
wpalafox <wpalafox@gmail.com>
2022-02-09 21:22:41 -0700

    completed popup vid

--- src/App.js
+++ src/App.js
@@ -1,23 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
+import Popup from './components/Popup';
+import { useState, useEffect } from 'react';
 
 function App() {
+
+  const [buttonPopup, setButtonPopup] = useState(false);
+  const [timedPopup, setTimedPopup] = useState(false);
+
+ 
+
+  useEffect(() =>{ 
+    setTimeout(() => {
+      setTimedPopup(true);
+    }, 4000);
+  }, []);
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <main>
+            <h1>React Popups</h1>
+            <br/><br/>
+            <button onClick={() => setButtonPopup(true)}>Open Popups</button>
+           
+        </main>
+        <Popup trigger={buttonPopup} setTrigger={setButtonPopup}>
+              <h3>my popup</h3>
+              <p>this is my button triggered popup</p>
+        </Popup>
+
+        <Popup trigger={timedPopup} setTrigger={setTimedPopup}>
+              <h3>my popup</h3>
+              <p>this is my button triggered popup</p>
+        </Popup>
     </div>
   );
 }
