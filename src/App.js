93c59158af6653f6cfe0e027d4aa6f19e4c48b5e
wpalafox <wpalafox@gmail.com>
2022-02-10 13:15:47 -0700

    successfully triggering popups in succession

--- src/App.js
+++ src/App.js
@@ -2,7 +2,8 @@ import Popup from './components/Popup';
 import { useState, useEffect } from 'react';
 
 function App() {
-
+  //declaring a new state variable. hooks
+  const [button2Popup, setButton2Popup] = useState(false);
   const [buttonPopup, setButtonPopup] = useState(false);
   const [timedPopup, setTimedPopup] = useState(false);
 
@@ -15,6 +16,20 @@ function App() {
   }, []);
 
 
+ function firstClick(){
+    setTimedPopup(false);
+    setButtonPopup(true);
+ }
+
+ function secondClick(){
+  setButtonPopup(false);
+  setButton2Popup(true);
+}
+
+  
+
+
+
   return (
     <div className="App">
         <main>
@@ -23,15 +38,25 @@ function App() {
             <button onClick={() => setButtonPopup(true)}>Open Popups</button>
            
         </main>
+        
+        <Popup trigger={button2Popup} setTrigger={setButton2Popup}>
+              <h3>my popup</h3>
+              <p>this is the 3rd popup</p>
+            
+        </Popup>
+              
         <Popup trigger={buttonPopup} setTrigger={setButtonPopup}>
               <h3>my popup</h3>
-              <p>this is my button triggered popup</p>
+              <p>this is the 2nd popup triggered popup</p>
+              <button onClick={secondClick}>Open Popups</button>
         </Popup>
 
         <Popup trigger={timedPopup} setTrigger={setTimedPopup}>
               <h3>my popup</h3>
-              <p>this is my button triggered popup</p>
+              <p>this is my first popup</p>
+              <button onClick={firstClick}>Open Popups</button>
         </Popup>
+       
     </div>
   );
 }
