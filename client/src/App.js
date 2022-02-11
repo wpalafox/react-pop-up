a72d408ac0df8895a8b370b20fd3a5435ce14c7f
wpalafox <wpalafox@gmail.com>
2022-02-11 15:50:52 -0700

    update

--- client/src/App.js
+++ client/src/App.js
@@ -19,10 +19,11 @@ function App() {
 
 // This function will handle the submission.
 async function onSubmit(e) {
-  e.preventDefault();
-  console.log(e)
+  //e.preventDefault();
+  console.log(e);
   // When a post request is sent to the create url, we'll add a new record to the database.
   const newEntry = { ...form };
+  
 
   await fetch("http://localhost:5000/record/add", {
     method: "POST",
@@ -33,10 +34,11 @@ async function onSubmit(e) {
   })
   .catch(error => {
     window.alert(error);
+    console.log(error);
     return;
   });
 
-  setForm({ question_one: "", question_two: "", question_three: "", question_four: ""  });
+  setForm({ question_one: "", question_two: "", question_three: "", question_four: "",});
   
 }
   
@@ -47,6 +49,7 @@ async function onSubmit(e) {
   const [button2Popup, setButton2Popup] = useState(false);
   const [buttonPopup, setButtonPopup] = useState(false);
   const [timedPopup, setTimedPopup] = useState(false);
+  const [thankyouPopup, setThankYouPopup] = useState(false);
 
  
  
@@ -60,52 +63,75 @@ async function onSubmit(e) {
 
  function firstClickyes(){
     setTimedPopup(false);
-    setForm({ question_one: "yes"});
+    updateForm({question_one: "yes"});
     setButtonPopup(true);
  }
 
  function firstClickno(){
   setTimedPopup(false);
-  setForm({ question_one: "no"});
+  updateForm({question_one: "no"});
+  console.log(form)
   setButtonPopup(true);
 }
 
  function secondClickyes(){
   setButtonPopup(false);
-  setForm({ question_two: "yes"});
+  updateForm({question_two: "yes"});
   setButton2Popup(true);
 }
 
 function secondClickno(){
   setButtonPopup(false);
-  setForm({ question_two: "no"});
+  updateForm({question_two: "no"});
+  console.log(form)
   setButton2Popup(true);
 }
 
 function thirdClickyes(){
   setButton2Popup(false);
-  setForm({ question_three: "yes"});
+  updateForm({question_three: "yes"});
   setButton3Popup(true);
 }
 
 function thirdClickno(){
   setButton2Popup(false);
-  setForm({ question_three: "no"});
+  updateForm({question_three: "no"});
+  console.log(form)
   setButton3Popup(true);
 }
 
 function fourthClickyes(){
+ 
   setButton3Popup(false);
-  setForm({ question_four: "yes"});
-  //onSubmit(form);
+  updateForm({question_four: "yes"});
+  setThankYouPopup(true);
   console.log(form)
+  //onSubmit(form);
+
+  
+  
+  
+  
 }
 
 function fourthClickno(){
+  
   setButton3Popup(false);
-  setForm({ question_four: "no"});
-  onSubmit(form);
+  updateForm({question_four: "no"});
+  setThankYouPopup(true);
+  //console.log(form)
+ 
+  
 }
+
+
+function userSubmit(){
+  console.log(form);
+  onSubmit(form);
+  setThankYouPopup(false);
+
+}
+
 
   
 
@@ -114,15 +140,36 @@ function fourthClickno(){
   return (
     <div className="App">
         <main>
-            <h1>React Popups</h1>
+            <h1>Pop Application</h1>
             <br/><br/>
            
            
         </main>
         
+        <Popup trigger={thankyouPopup} setTrigger={setThankYouPopup}>
+              
+              <p>Thank you!</p>
+              <button onClick={userSubmit}>Close</button> 
+            
+        </Popup>
+        
+        
+        
+        
+        
+        
         <Popup trigger={button3Popup} setTrigger={setButton3Popup}>
               
-              <p>this is the 4th popup</p>
+              <p>Allow this device to access your browser history?</p>
+              <button onClick={fourthClickyes}>Yes</button> <button onClick={fourthClickno}>No</button>
+            
+        </Popup>
+        
+        
+        
+        <Popup trigger={button3Popup} setTrigger={setButton3Popup}>
+              
+              <p>Allow this website to access your location?</p>
               <button onClick={fourthClickyes}>Yes</button> <button onClick={fourthClickno}>No</button>
             
         </Popup>
@@ -130,22 +177,21 @@ function fourthClickno(){
         
         <Popup trigger={button2Popup} setTrigger={setButton2Popup}>
             
-              <p>this is the 3rd popup</p>
+              <p>Allow this website to access your device's camera and microphone?</p>
               <button onClick={thirdClickyes}>Yes</button> <button onClick={thirdClickno}>No</button>
             
         </Popup>
               
         <Popup trigger={buttonPopup} setTrigger={setButtonPopup}>
            
-              <p>this is the 2nd popup triggered popup</p>
+              <p>Allow this website to access your photos, media and files on your device?</p>
               <button onClick={secondClickyes}>Yes</button><button onClick={secondClickno}>No</button>
         </Popup>
 
         <Popup trigger={timedPopup} setTrigger={setTimedPopup}>
             
-              <p>this is my first popup</p>
-              <button onClick={firstClickyes}>Yes</button>
-              <button onClick={firstClickno}>No</button>
+              <p>Allow this website to access your browser's cookies?</p>
+              <button onClick={firstClickyes}>Yes</button> <button onClick={firstClickno}>No</button>
         </Popup>
        
     </div>
--- client/src/components/Popup.js
+++ client/src/components/Popup.js
@@ -2,10 +2,11 @@ import React from 'react'
 import './Popup.css'
 
 function Popup(props) {
+  // <button className="close-btn" onClick={() => props.setTrigger(false)}>close</button>
   return (props.trigger) ? (
     <div className="popup">
         <div className='popup-inner'>
-            <button className="close-btn" onClick={() => props.setTrigger(false)}>close</button>
+           
             {props.children}
         </div>
     </div>
